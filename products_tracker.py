e4fbe055ead3dc4fcbfa5909f5d8d59841426099
DavidSalasGimeno <david@salas.one>
2021-11-30 17:35:27 +0100

    Html getter

---
+++ products_tracker.py
@@ -0,0 +1,38 @@
+import requests 
+from time import sleep
+from bs4 import BeautifulSoup
+
+
+
+def getHtml(url):  
+
+    headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.amazon.com/',        
+    }
+
+    # Download the page using requests
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+
+    # Simple check to check if page was blocked (Usually 503)
+    if 500 < r.status_code:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    # Parsing html
+    html = BeautifulSoup(r.text,'html.parser')
+
+    
+    return html
+
+    
